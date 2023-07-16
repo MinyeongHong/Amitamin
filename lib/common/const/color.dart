206464647a62f364a4f96241b18de842883cd63f
MinyeongHong <agka00161@gmail.com>
2023-07-16 16:20:53 +0900

    add color component

---
+++ lib/common/const/color.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color primaryBlue0 = Color(0xFF05A8BC);
+  static const Color primaryBlue100 = Color(0xFF26C1D6);
+  static const Color primaryBlue200 = Color(0xFF7FDBE8);
+  static const Color primaryBlue300 = Color(0xFFABF1FB);
+  static const Color primaryBlue400  = Color(0xFFDBF9FD);
+
+  static const Color physicalRegular = Color(0xFFF8AC45);
+  static const Color physicalLight = Color(0xFFF8E3C7);
+  static const Color physicalExtraLight = Color(0xFFFFF6EA);
+
+  static const Color sleepRegular = Color(0xFF8E91BE);
+  static const Color sleepLight = Color(0xFFD2D5FF);
+  static const Color sleepExtraLight = Color(0xFFF2F4FF);
+
+  static const Color nutriRegular = Color(0xFF6AAFA1);
+  static const Color nutriLight = Color(0xFFCAECE5);
+  static const Color nutriExtraLight = Color(0xFFF0F6F6);
+
+  static const Color mentalRegular = Color(0xFFE389B5);
+  static const Color mentalLight = Color(0xFFFFC7E2);
+  static const Color mentalExtraLight = Color(0xFFFBF0F6);
+
+  static const Color backGround = Color(0xFFF7F7F7);
+
+  static const Color error = Color(0xFFFC5F7C);
+  static const Color success = Color(0xFF18A0FB);
+  static const Color fail = Color(0xFFFF7979);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color extraLightGray = Color(0xFFEEEEEE);
+  static const Color lightGray = Color(0xFFE0E0E0);
+  static const Color gray = Color(0xFFBDBDBD);
+  static const Color extraDarkGray = Color(0xFF9E9E9E);
+  static const Color darkGray = Color(0xFF757575);
+  static const Color lightBlack = Color(0xFF424242);
+  static const Color black = Color(0xFF111111);
+
+}
